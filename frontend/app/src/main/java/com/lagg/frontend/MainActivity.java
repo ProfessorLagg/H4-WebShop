dc50aeb802a80aa09921adfb5e57fb925c377ecc
ProfessorLagg <futurecake2999@gmail.com>
2024-09-12 20:18:09 +0200

    Still http eludes me

--- frontend/app/src/main/java/com/lagg/frontend/MainActivity.java
+++ frontend/app/src/main/java/com/lagg/frontend/MainActivity.java
@@ -1,7 +1,7 @@
 package com.lagg.frontend;
 
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.ViewGroup;
 
@@ -12,11 +12,27 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.lagg.frontend.model.Category;
 import com.lagg.frontend.net.DataFetcher;
+import com.lagg.frontend.net.HttpRequest;
+import com.lagg.frontend.net.HttpResponse;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.Callable;
 import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 
 public class MainActivity extends AppCompatActivity {
 		private static final String TAG = "MainActivity";
@@ -42,13 +58,86 @@ public class MainActivity extends AppCompatActivity {
 		}
 
 		private void displayPageCategories() {
-				ArrayList<Category> categories = new ArrayList<>(DataFetcher.getAllCategories());
+				ArrayList<Category> categories = new ArrayList<>(this.getAllCategories());
 				pageLayout.removeAllViews();
 
 				for (Category category : categories) {
 						Log.d(TAG, "displayPageCategories: Creating banner for category " + category.toString());
 						CategoryBanner banner = new CategoryBanner(pageLayout.getContext(), category);
 						pageLayout.addView(banner);
+				}
+		}
+
+		private byte[] getRequest(URL url) {
+				try {
+						Callable<byte[]> callable = () -> {
+								try {
+
+										HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+										InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+										byte[] buffer = new byte[in.available()]; /* 1073741824 bytes == 1gb */
+										int readsize = in.read(buffer);
+										urlConnection.disconnect();
+										return buffer;
+								} catch (Exception e) {
+										Log.d(TAG, "getAllCategories: ", e);
+										return new byte[0];
+								}
+						};
+
+						FutureTask<byte[]> promise = new FutureTask<>(callable);
+						promise.run();
+						return promise.get();
+				} catch (Exception e) {
+						Log.d(TAG, "getRequest: ", e);
+						return new byte[0];
+				}
+		}
+
+		private static List<Category> parseCategoryArray(String jsonString) {
+				if (Utils.isNullOrWhitespace(jsonString)) { return Collections.emptyList(); }
+				ArrayList<Category> result = new ArrayList<>();
+				try {
+						JSONArray jarr = new JSONArray(jsonString);
+						int len = jarr.length();
+						for (int i = 0; i < len; i++) {
+								try {
+										JSONObject jobj = jarr.getJSONObject(i);
+										Category category = new Category();
+										category.loadJson(jobj);
+										result.add(category);
+								} catch (JSONException e) {
+										Log.e(TAG, "parseCategoryArray: could not parse item at index " + i + " in '" + jsonString +
+														"' as a Category");
+								}
+						}
+				} catch (Exception e) {
+						Log.e(TAG, "parseCategoryArray: ", e);
+						return Collections.emptyList();
+				}
+				return result;
+		}
+
+		/** Gets all the categories from the webserver */
+		public List<Category> getAllCategories() {
+				Resources resources = getResources();
+				String serverHostName = resources.getString(R.string.serverHostName);
+				int requestTimeout = resources.getInteger(R.integer.requestTimeout);
+
+				String categoryHttpPath = resources.getString(R.string.categoryHttpPath);
+				boolean useHttps = resources.getBoolean(R.bool.use_https);
+
+				String url = Utils.getUrlString(serverHostName, categoryHttpPath, useHttps);
+				HttpRequest request = new HttpRequest(url);
+				FutureTask<HttpResponse> responseFuture = request.getTask();
+				this.executor.execute(responseFuture);
+				try {
+						HttpResponse response = responseFuture.get();
+						String jsonString = new String(response.body, StandardCharsets.UTF_8);
+						return parseCategoryArray(jsonString);
+				} catch (Exception e) {
+						Log.e(TAG, "getAllCategories: error trying to get or parse HttpResponse", e);
+						return Collections.emptyList();
 				}
 		}
 }
--- frontend/app/src/main/java/com/lagg/frontend/Utils.java
+++ frontend/app/src/main/java/com/lagg/frontend/Utils.java
@@ -2,138 +2,169 @@ package com.lagg.frontend;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Executor;
 
 import kotlin.text.UStringsKt;
 
 public class Utils {
-	public static String trimStart(String s, char c) {
-		String S = s + "";
-		while (S.charAt(0) == c) {
-			S = S.substring(1);
-		}
-		return S;
-	}
-
-	public static String trimEnd(String s, char c) {
-		String S = s + "";
-		while (S.charAt(s.length() - 1) == c) {
-			S = S.substring(0, s.length() - 1);
-		}
-		return S;
-	}
-
-	public static String trim(String s, char c) {
-		String S = s + "";
-		while (S.charAt(s.length() - 1) == c) {
-			S = S.substring(0, s.length() - 1);
-		}
-		while (S.charAt(0) == c) {
-			S = S.substring(1);
-		}
-		return S;
-	}
-
-	public static int clamp(int value, int min, int max) {
-		return Math.max(min, Math.min(value, max));
-	}
-
-	private static class SyncRunnable implements Runnable {
-		private boolean started;
-
-		public boolean isStarted() {
-			return this.started;
+		public static String trimStart(String s, char c) {
+				String S = s + "";
+				while (S.charAt(0) == c) {
+						S = S.substring(1);
+				}
+				return S;
 		}
 
-		private boolean finished;
-
-		public boolean isFinished() {
-			return this.finished;
+		public static String trimEnd(String s, char c) {
+				String S = s + "";
+				while (S.charAt(s.length() - 1) == c) {
+						S = S.substring(0, s.length() - 1);
+				}
+				return S;
 		}
 
-		public final Runnable runnable;
-
-		public SyncRunnable(Runnable runnable) {
-			this.started = false;
-			this.finished = false;
-			this.runnable = runnable;
+		public static String trim(String s, char c) {
+				String S = s + "";
+				while (S.charAt(s.length() - 1) == c) {
+						S = S.substring(0, s.length() - 1);
+				}
+				while (S.charAt(0) == c) {
+						S = S.substring(1);
+				}
+				return S;
 		}
 
-		@Override
-		public void run() {
-			this.started = true;
-			this.finished = false;
-			runnable.run();
-			this.finished = true;
-		}
-	}
-
-	public static void executeSync(Executor executor, Runnable runnable) {
-		SyncRunnable syncRunnable = new SyncRunnable(runnable);
-		executor.execute(syncRunnable);
-		while (!syncRunnable.isStarted() && !syncRunnable.isFinished()) {
-		}
-	}
-
-	public static String getUrlString(String host, String path, boolean useHttps) {
-		return getUrlString(host, path, useHttps, null);
-	}
-
-	public static String getUrlString(String host, String path, boolean useHttps, Map<String, String> urlParams) {
-		String h = Utils.trim(host.trim(), '/');
-		String p = Utils.trim(path.trim(), '/');
-		String url = (useHttps ? "https://" : "http://") + h + '/' + p;
-
-		if (urlParams != null && !urlParams.isEmpty()) {
-			url += getParamsString(urlParams, false);
+		public static int clamp(int value, int min, int max) {
+				return Math.max(min, Math.min(value, max));
 		}
 
-		return url;
-	}
+		private static class SyncRunnable implements Runnable {
+				private boolean started;
 
-	public static String getParamsString(Map<String, String> parameters) {
-		return getParamsString(parameters, false);
-	}
+				public boolean isStarted() {
+						return this.started;
+				}
 
-	public static String getParamsString(Map<String, String> parameters, boolean urlEncode) {
-		StringBuilder sb = new StringBuilder();
-		char prependChar = '?';
-		for (String k : parameters.keySet()) {
-			String v = parameters.get(k);
-			sb.append(prependChar);
-			sb.append(k);
-			sb.append('=');
-			sb.append(v);
-			if (prependChar == '?') {
-				prependChar = '&';
-			}
+				private boolean finished;
+
+				public boolean isFinished() {
+						return this.finished;
+				}
+
+				public final Runnable runnable;
+
+				public SyncRunnable(Runnable runnable) {
+						this.started = false;
+						this.finished = false;
+						this.runnable = runnable;
+				}
+
+				@Override
+				public void run() {
+						this.started = true;
+						this.finished = false;
+						runnable.run();
+						this.finished = true;
+				}
 		}
-		String paramsString = sb.toString();
-		String result = paramsString;
-		if (urlEncode) {
-			// SAFE TO IGNORE UnsupportedEncodingException
-			try {
-				result = URLEncoder.encode(paramsString, "UTF-8");
-			} catch (UnsupportedEncodingException e) {
-			}
+
+		public static void executeSync(Executor executor, Runnable runnable) {
+				SyncRunnable syncRunnable = new SyncRunnable(runnable);
+				executor.execute(syncRunnable);
+				while (!syncRunnable.isStarted() && !syncRunnable.isFinished()) {
+				}
 		}
 
-		return result;
-	}
+		public static String getUrlString(String host, String path, boolean useHttps) {
+				return getUrlString(host, path, useHttps, null);
+		}
 
-	public static byte[] concatWithArrayCopy(byte[] array1, byte[] array2) {
-		byte[] result = Arrays.copyOf(array1, array1.length + array2.length);
-		System.arraycopy(array2, 0, result, array1.length, array2.length);
-		return result;
-	}
+		public static String getUrlString(String host, String path, boolean useHttps, Map<String, String> urlParams) {
+				String h = Utils.trim(host.trim(), '/');
+				String p = Utils.trim(path.trim(), '/');
+				String url = (useHttps ? "https://" : "http://") + h + '/' + p;
 
-	public static byte[] slice(byte[] arr, int start, int end) {
-		int len = end - start;
-		byte[] result = new byte[len];
-		System.arraycopy(arr, start, result, 0, len);
-		return result;
-	}
+				if (urlParams != null && !urlParams.isEmpty()) {
+						url += getParamsString(urlParams, false);
+				}
+
+				return url;
+		}
+
+		public static String getParamsString(Map<String, String> parameters) {
+				return getParamsString(parameters, false);
+		}
+
+		public static String getParamsString(Map<String, String> parameters, boolean urlEncode) {
+				StringBuilder sb = new StringBuilder();
+				char prependChar = '?';
+				for (String k : parameters.keySet()) {
+						String v = parameters.get(k);
+						sb.append(prependChar);
+						sb.append(k);
+						sb.append('=');
+						sb.append(v);
+						if (prependChar == '?') {
+								prependChar = '&';
+						}
+				}
+				String paramsString = sb.toString();
+				String result = paramsString;
+				if (urlEncode) {
+						// SAFE TO IGNORE UnsupportedEncodingException
+						try {
+								result = URLEncoder.encode(paramsString, "UTF-8");
+						} catch (UnsupportedEncodingException e) {
+						}
+				}
+
+				return result;
+		}
+
+		public static byte[] concatWithArrayCopy(byte[] array1, byte[] array2) {
+				byte[] result = Arrays.copyOf(array1, array1.length + array2.length);
+				System.arraycopy(array2, 0, result, array1.length, array2.length);
+				return result;
+		}
+
+		public static byte[] slice(byte[] arr, int start, int end) {
+				int len = end - start;
+				byte[] result = new byte[len];
+				System.arraycopy(arr, start, result, 0, len);
+				return result;
+		}
+
+		public static List<String> split(String str, String sep) {
+				ArrayList<String> result = new ArrayList<>();
+				int sepidx = str.indexOf(sep);
+				while (sepidx > 0) {
+						String sub = str.substring(0, sepidx);
+						result.add(sub);
+						str = str.substring(sub.length());
+						sepidx = str.indexOf(sep);
+				}
+				if (!str.isEmpty()) { result.add(str); }
+				return result;
+		}
+		private static final char[] whitespaceChars =
+						new char[]{0x09 /* Horizontal Tab */, 0x0A /* Line Feed */, 0x0B /* Vertical Tabulation */, 0x0C
+										/* Form Feed */, 0x0D /* Carriage Return */, 0x20 /* Space */,};
+		public static boolean isWhitespaceChar(char c) {
+				for (int i = 0; i < whitespaceChars.length; i++) {
+						if (c == whitespaceChars[i]) { return true; }
+				} return false;
+		}
+		public static boolean isNullOrEmpty(String str) {
+				return str == null || str.isEmpty();
+		}
+		public static boolean isNullOrWhitespace(String str) {
+				if (isNullOrEmpty(str)) { return true; } for (char c : str.toCharArray()) {
+						if (!isWhitespaceChar(c)) { return false; }
+				} return true;
+		}
 }
